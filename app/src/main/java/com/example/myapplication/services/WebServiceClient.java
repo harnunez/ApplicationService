40e0565fc79ff81d3fa3416e6364284009f4239f
unknown <harnunez@gmail.com>
2021-01-06 00:41:45 -0300

    Se finalizo con las actividades ordenadas/ Queda pendiente ordenar la lista

--- app/src/main/java/com/example/myapplication/services/WebServiceClient.java
+++ app/src/main/java/com/example/myapplication/services/WebServiceClient.java
@@ -4,9 +4,13 @@ import com.example.myapplication.model.Data;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Url;
 
 public interface WebServiceClient {
 
     @GET("people")
     Call<Data>getPersonajes();
+
+    @GET()
+    Call<Data>getPersonajes(@Url String url);
 }
